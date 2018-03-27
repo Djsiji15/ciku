ba839fe166854effdf35d3a495c459b2bf9ce7d1
Van Holmes <32923127+Djsiji15@users.noreply.github.com>
2018-03-27 15:49:17 +0800

    v1.0
    Add the config file.

--- ciku.sh
+++ ciku.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+touch base.txt
+
+if [ -e config.txt ];then    #if config file not exist, creative it
+  echo "reading the file"
+else
+  touch config.txt
+  echo "creating a new config file"
+  echo "words=0" >> config.txt  
+fi
+
+num=`sed '/^words=/!d;s/.*=//' config.txt`  #read the config file
+if [ $num -g 0 ]; then
+	echo "all words are:"$num
+else
+	echo "no word in ciku"
+fi
+
 echo  "welcome to the ciku"
 echo  "plz add the word,press q to quit"
 touch base.txt;
